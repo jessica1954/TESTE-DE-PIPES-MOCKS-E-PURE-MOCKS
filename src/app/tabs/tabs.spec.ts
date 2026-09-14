ea92cc047abbebf601433b27f97f2c43932ece83
jessica1954 <jessica.a.jesus@ba.estudante.senai.br>
2026-09-14 16:13:42 -0300

    Adicionar

--- src/app/tabs/tabs.spec.ts
+++ src/app/tabs/tabs.spec.ts
@@ -1,6 +1,10 @@
-import { DebugElement } from '@angular/core';
+import { Component, DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { beforeEach, describe, it, expect, vi } from 'vitest';
+import { TabsComponent } from './tabs';
+import { TabData } from './tabs.model';
+import { MOCK_TABS } from '../testing/testing-data';
+import { By } from '@angular/platform-browser';
 
 describe('TabsComponent', () => {
   let component: TabsComponent;
@@ -12,9 +16,44 @@ describe('TabsComponent', () => {
     await TestBed.configureTestingModule({
       imports: [TabsComponent],
     }).compileComponents();
+    fixture = TestBed.createComponent(TabsComponent);
     component = fixture.componentInstance;
     de = fixture.debugElement;
     fixture.componentRef.setInput('tabs', mockTabs);
     fixture.detectChanges();
+  });
+
+  it('should create the tabs component', () => {
+    expect(component).toBeDefined();
+  });
+
+  it('should render corret name off tab buttons', () => {
+    const buttons = de.queryAll(By.css('.tab-link'));
+    expect(buttons.length).toBe(2);
+    expect(buttons[0].nativeElement.textContent.trim()).toBe('Beginner');
+    expect(buttons[1].nativeElement.textContent.trim()).toBe('Advanced');
+  });
+
+  it('should apply the active class to the selected tab', () => {
+    fixture.componentRef.setInput('activeTab', 'advanced');
+    fixture.detectChanges();
+    const button = de.query(By.css('.tab-link:last-child'));
+    expect(button.nativeElement.classList).toContain('active');
+  });
+
+  it('should emit acvtiveTab when a tab is clicked', () => {
+    const button = de.query(By.css('.tab-link:last-child'));
+    button.nativeElement.click();
+    fixture.detectChanges();
+    expect(component.activeTab()).toBe('advanced');
+  });
+
+  it('should emit tabChanged when a tab is clicked', () => {
+    const emitSpy = vi.spyOn(component.tabChanged, 'emit');
+    const button = de.query(By.css('.tab-link:last-child'));
+    button.nativeElement.click();
+    fixture.detectChanges();
+    expect(emitSpy).toHaveBeenCalledWith('advanced');
+    expect(emitSpy).toHaveBeenCalledOnce();
   });
 });
