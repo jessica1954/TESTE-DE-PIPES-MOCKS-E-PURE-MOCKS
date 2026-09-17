41ea080f34e302b94d72c82e92cc367209d5b8f0
jessica1954 <jessica.a.jesus@ba.estudante.senai.br>
2026-09-17 14:49:38 -0300

    atualizacao 1.1.2

--- src/app/services/course-resolver.spec.ts
+++ src/app/services/course-resolver.spec.ts
@@ -1,24 +1,51 @@
-import { TestBed } from "@angular/core/testing";
-import { beforeEach, describe, vi } from "vitest";
-import { CoursesService } from "./courses.service";
-import {CoursePage } from "../course-page/course-page";
+import { TestBed } from '@angular/core/testing';
+import { beforeEach, describe, vi, it, expect } from 'vitest';
+import { CoursesService } from './courses.service';
+import { CoursePage } from '../course-page/course-page';
+import { provideRouter, Router } from '@angular/router';
+import { RouterTestingHarness } from '@angular/router/testing';
+import { courseResolver } from './course.resolver';
+import { MOCK_COURSES } from '../testing/testing-data';
 
+describe('CourseResolver', () => {
+  let mockCoursesService: any;
+  let harness: RouterTestingHarness;
 
-describe ('CourseResolver', () => {
-    let mockCoursesService:any;
+  beforeEach(async () => {
+    mockCoursesService = {
+      findCourseById: vi.fn(),
+    };
 
-    beforeEach(async ()=> {
-        mockCoursesService = {
-            findCourseById: vi.fn()
-        }
+    await TestBed.configureTestingModule({
+      imports: [CoursePage],
+      providers: [
+        { provide: CoursesService, useValue: mockCoursesService },
+        provideRouter([
+          {
+            path: 'courses/:id',
+            component: CoursePage,
+            resolve: {
+              course: courseResolver,
+            },
+          },
+        ]),
+      ],
+    }).compileComponents();
 
-        await TestBed.configureTestingModule({
-        imports: [CoursePage],
-        providers: [
-            {provide: CoursesService, useValue: mockCoursesService},
-        ]
-}).compileComponents();
+    harness = await RouterTestingHarness.create();
+  });
 
-})
+  it('Should load correct course by ID', async () => {
+    mockCoursesService.findCourseById.mockResolvedValueOnce(MOCK_COURSES[0]);
+    const component = await harness.navigateByUrl('/courses/1', CoursePage);
 
-})
+    //ESPERO QUE SEJA INJETADA NA ROTA A URL CORRETA
+    expect(TestBed.inject(Router).url).toBe('/courses/1');
+
+    //ESPERO QUE TENHA UMA CHAMADA EM "FindCourseByID" PARA  O "ID 1"
+    expect(mockCoursesService.findCourseById).toHaveBeenCalledOnce();
+    expect(mockCoursesService.findCourseById).toHaveBeenCalledWith('1');
+
+    expect(harness.routeNativeElement?.textContent).toContain('Beginner Course');
+  });
+});
