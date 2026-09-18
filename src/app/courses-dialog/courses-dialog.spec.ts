760e8bd14c5db912abe3584c994a81433a4e2641
jessica1954 <jessica.a.jesus@ba.estudante.senai.br>
2026-09-18 12:56:01 -0300

    modificacoes

--- src/app/courses-dialog/courses-dialog.spec.ts
+++ src/app/courses-dialog/courses-dialog.spec.ts
@@ -9,7 +9,7 @@ import { DebugElement } from '@angular/core';
 import { clickButton } from '../testing/testing-utils';
 import { FieldState } from '@angular/forms/signals';
 
-describe('CoursesDialog', async () => {
+describe('CoursesDialog', () => {
   let component: CoursesDialog;
   let fixture: ComponentFixture<CoursesDialog>;
   let de: DebugElement;
@@ -20,67 +20,92 @@ describe('CoursesDialog', async () => {
     mockCoursesService = {
       saveCourse: vi.fn().mockResolvedValue({}),
     };
+
     mockDialogRef = {
       close: vi.fn(),
     };
+
+    await TestBed.configureTestingModule({
+      imports: [CoursesDialog],
+      providers: [
+        {
+          provide: CoursesService,
+          useValue: mockCoursesService,
+        },
+        {
+          provide: DialogRef,
+          useValue: mockDialogRef,
+        },
+        {
+          provide: DIALOG_DATA,
+          useValue: {
+            course: MOCK_COURSES[0],
+          },
+        },
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CoursesDialog);
+    de = fixture.debugElement;
+    component = fixture.componentInstance;
+
+    fixture.detectChanges();
   });
-
-  await TestBed.configureTestingModule({
-    imports: [CoursesDialog],
-    providers: [
-      { provide: CoursesService, useValue: mockCoursesService },
-      { provide: DialogRef, useValue: mockDialogRef },
-      { provide: DIALOG_DATA, useValue: { course: MOCK_COURSES[0] } },
-    ],
-  }).compileComponents();
-
-  fixture = TestBed.createComponent(CoursesDialog);
-  de = fixture.debugElement;
-  component = fixture.componentInstance;
-  fixture.detectChanges();
 
   it('should initialize the form with course data', () => {
     expect(component.courseForm.description().value()).toBe('Beginner Course');
 
     expect(component.courseForm.category().value()).toBe('BEGINNER');
+
     expect(component.courseForm.releasedAt().value()).toBe(new Date().toLocaleDateString('pt-BR'));
+
     expect(component.courseForm.longDescription().value()).toBe('Theory.');
-    expect(component.courseForm().valid()).toBe('true');
+
+    expect(component.courseForm().valid()).toBe(true);
   });
 
   it('should call saveCourse and close dialog', async () => {
     component.courseForm.description().value.set('New Course Title');
+
     fixture.detectChanges();
 
     clickButton(de, '.btn-primary');
+
     await fixture.whenStable();
 
-    expect.objectContaining({
-      titles: expect.objectContaining({ description: 'New Course Title' }),
-    });
+    expect(mockCoursesService.saveCourse).toHaveBeenCalled();
+
     expect(mockDialogRef.close).toHaveBeenCalled();
   });
 
   it('should handle all form field errors', () => {
-    testFieldError(component.courseForm.description(), '.description', 'Description is requerid');
-    testFieldError(component.courseForm.category(), '.category', 'Category is requerid');
-    testFieldError(component.courseForm.releasedAt(), '.released-at', 'Released Date is requerid');
+    testFieldError(component.courseForm.description(), '.description', 'Description is required');
+
+    testFieldError(component.courseForm.category(), '.category', 'Category is required');
+
+    testFieldError(component.courseForm.releasedAt(), '.released-at', 'Released Date is required');
+
     testFieldError(
       component.courseForm.longDescription(),
       '.long-description',
       'Long Description is required',
     );
   });
+
   function testFieldError(fieldState: FieldState<any>, selector: string, message: string) {
     fieldState.value.set('');
     fieldState.markAsTouched();
+
     fixture.detectChanges();
 
     const errorList = de.query(By.css(`${selector} .error-list`));
+
     expect(errorList).toBeTruthy();
+
     expect(errorList.nativeElement.textContent).toContain(message);
 
     const saveBtn = de.query(By.css('.btn-primary'))?.nativeElement;
+
     expect(saveBtn?.disabled).toBe(true);
   }
 });
